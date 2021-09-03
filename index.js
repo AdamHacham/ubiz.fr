a907a181ce595fc0fd1593222469e2fb4f5ced99
Adam Hacham <adamhacham2@gmail.com>
2021-09-03 21:02:11 +0200

    Etape 3 - ESlint Fix Syntax Error Script

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 const express = require('express')
 const app = express()
 
-
-app.get('/', function(req, res) {
-   return res.send('Hello!');
+app.get('/', function (req, res) {
+  return res.send('Hello!')
 })
 
-
-app.listen(3000, function() {
-   console.log('server running at port 3000');
-
+app.listen(3000, function () {
+  console.log('server running at port 3000')
 })
-
