7e519e78739c647af488bdf100ce7463d0718126
Adam Hacham <adamhacham2@gmail.com>
2021-09-05 20:49:52 +0200

    Tache 8 - Joi & Validation des errors

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express')
 const bodyParser = require('body-parser')
 require('./utils/db.config')
 
-
 const authRoutes = require('./routes/authRoutes')
 const app = express()
 
